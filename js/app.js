08e0a0fb7bbc8395dc791909c2acd34fe1cad1c4
Susana160 <susa.016@hotmail.com>
2020-11-27 01:16:03 -0500

    first commit donde estoy creando una página básica con css imitación netflix

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// alert("hello");
+
+// //javascript, jQuery
+
+// var xhr = ("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=d6CdUMQ8lGZWSwy1L79GDsnQdfE0HAXI&limit=5");
+
+
+// function crear(){
+//     let div = document.createElement("div");
+//     div.className = "internalDiv";
+
+//     let img = document.createElement("img");
+//     img.src = xhr;
+
+//     div.appendChild(img);
+// };
